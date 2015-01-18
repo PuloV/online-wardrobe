6dcf66200cdcbfb96a089da2cc89553663862760
yordan pulov <yordanpulov@gmail.com>
2015-01-18 14:53:47 +0200

    menu scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,38 @@
 var App = function () {}
 
+App.bindMenuLinks = function(){
+	$(".sideBar_link").click(function (e) {
+		e.preventDefault();
+		link = $(this).attr("href");
+		switch(link){
+			case "#develop" :
+				subnav = $(this).parent().find("ul.subnav")
+				console.log(subnav)
+				subnav.removeClass("hidden");
+				$(".nav.navbar-nav > li").removeClass("active")
+				$(link).addClass("active")
+				break;
+			default:
+				$(".nav.navbar-nav > li").removeClass("active")
+				$(link).addClass("active")
+				break;
+		}
+	})
+}
 App.showMenu = function(type) {
 	$("#sideBar").load("side_bar.html");
-	App.load($("#sideBar") , "side_bar.html" , App.resizeMenu)
+
+	bind_links = function () {
+		App.resizeMenu();
+		App.bindMenuLinks();
+	}
+
+	App.load($("#sideBar") , "side_bar.html" , bind_links)
 
 }
 // binds the button for resizing the sidebar menu
 App.resizeMenu = function(type) {
-	$(".subnav").removeCss("hidden");
+
 	$('#collapse-sidebar').click(function(){
 		$('.navbar-side').toggleClass('navbar-side-closed');
 	    $('#wrapper').toggleClass('wrapper-full');
