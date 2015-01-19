bf29ec9df30e663d31152b4d7bb10230ed066fd5
yordan pulov <yordanpulov@gmail.com>
2015-01-19 20:25:07 +0200

    Shopping Cart

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 var App = function () {}
 
+App.shopingCartList = []
+
 //  binds the sidebar menu links
 App.bindMenuLinks = function(){
 	$(".sideBar_link").click(function (e) {
@@ -183,16 +185,103 @@ App.buildTypeClothesRow = function(cloth){
     return html
 }
 
+App.buildClothesItemRow = function (cloth) {
+	var html = ""
+	    html += "<div class='row imgfloatleft one_item' data-img='"+cloth.image+"' data-name='"+cloth.title+"' data-price='"+cloth.price+"'>"
+        html += "      <div class='col-xs-3 col-sm-5 imgnopadding'>"
+        html += "             <div class='imgVA'>"
+        html += "                 <img src='"+cloth.image+"'>"
+        html += "             </div>"
+
+        html += "     </div>"
+        html += "    <div class='col-xs-8 col-sm-6 infoBoxSpace'>"
+        html += "        <div class='panel panel-default noborder'>"
+        html += "             <div class='panel-heading '><h3>"+cloth.title+"</h3></font></div>"
+        html += "                 <div class='panel-body no-padding'>"
+        html += "                    <span class='label "+cloth.inStockLabel+"'>"+cloth.inStockTitle+"</span>"
+        html += "                        <div class='jumbotron color-cloud'>"
+        html += "                          <span>" +cloth.desc+"</span> <br />"
+        html += "                          <a class='btn btn-primary btn-pulldown'>Buy for "+cloth.price+"</a>"
+
+        html += "                       </div>  "
+        html += "                 </div>"
+       	html += "         </div>"
+        html += "    </div>"
+        html += "</div>"
+        return html
+}
+
 App.bindTypeClothesRowLink = function(){
 	$(".type_item").click(function(e){
 		var link = $(this).data("id")
 		console.log("a")
-		var callback = function () {
-			// here call App.getData  see line  51
+		callback = function () {
+			App.getData(link , function (data) {
+				var html = ""
+				data.forEach(function(element){
+					html += App.buildClothesItemRow(element);
+				})
+				$(".container .panel-body").html(html)
+				switch(link){
+					case "clothes/dresses":
+						secondBC = "Female Clothes"
+						thirdBC = "Dresses"
+					break;
+
+					case "clothes/shirts":
+						secondBC = "Male Clothes"
+						thirdBC = "Shirts"
+					break;
+
+					case "clothes/jeans":
+						secondBC = "Male Clothes"
+						thirdBC = "Jeans"
+					break;
+
+					case "clothes/tops":
+						secondBC = "Female Clothes"
+						thirdBC = "Tops"
+					break;
+				}
+				$(".container .breadcrumb .active").text(thirdBC)
+				$(".container .breadcrumb li").eq(1).text(secondBC)
+				App.bindItemBuy();
+			})
 		}
-		App.getData(link ,function (data) {
-			// each + print func like line 54
-		})
-		App.load($(".container") , "MaleShirts.html" , callback)
+		App.load($(".container") , "items.html" , callback)
 	})
 }
+
+App.bindItemBuy = function() {
+	$(".one_item").click(function() {
+		var img  = $(this).data("img")
+		var name = $(this).data("name")
+		var price = $(this).data("price")
+		App.shopingCartList.push({"image":img , "name":name, "price":price});
+		console.log(App.shopingCartList);
+	})
+};
+
+App.shopingCart = function() {
+	$(".shoping-cart").click(function (e) {
+
+		if(App.shopingCartList.length > 0){
+			$(".shoping-cart-list").html("");
+			App.shopingCartList.forEach(function(element){
+
+				var li = $(".hidden.list-group-item.shoping-cart-list-item").clone()
+				$(".product_img",li).attr("src",element.image);
+				$(".product_name",li).text(element.name +" " + element.price);
+				li.removeClass("hidden");
+				$(".shoping-cart-list").append(li);
+			})
+
+		}
+
+        $('#myModal').modal('show')
+        $(this).addClass("open-cart");
+    })
+    $('#myModal').on("hidden.bs.modal",function (argument) {
+        $(".shoping-cart").removeClass("open-cart");
+    })
+};
